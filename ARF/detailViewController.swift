89c8676df57d3e1983f6429bc7b0890b47c2dabd
滑川裕里瑛 <lucky854854@gmail.com>
2023-02-04 23:26:36 +0900

    特に変えてない！ URLとimageが、それぞれ違うものを表示できない

--- ARF/detailViewController.swift
+++ ARF/detailViewController.swift
@@ -19,11 +19,11 @@ class detailViewController: UIViewController {
     ]
     
     let URL = [
-    NSURL(string: "https://shiro-shiro.jp/item/12716.html"),
-    NSURL(string: "https://francfranc.com/products/1105120176402"),
-    NSURL(string: "https://francfranc.com/products/1105120217709")
-         ]
-
+        NSURL(string: "https://shiro-shiro.jp/item/12716.html"),
+        NSURL(string: "https://francfranc.com/products/1105120176402"),
+        NSURL(string: "https://francfranc.com/products/1105120217709")
+    ]
+    
     
     var index = 0
     
@@ -34,35 +34,37 @@ class detailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        
         
         nameLabel.text = String(roomfreglance[index])
         priceLabel.text = String(price[index])
+        
+        
         //        選んだcellによってimageをそれぞれ変えたい
         //        imageImage.image = UIImage(data: imageImage)
-//        URLButton.buttonType = String(URL[index])
+        //        URLButton.buttonType = String(URL[index])
         
-//      nameLabelとpriceLabelの文字サイズを自動変更する
+        //      nameLabelとpriceLabelの文字サイズを自動変更する
         nameLabel.adjustsFontSizeToFitWidth = true
         priceLabel.adjustsFontSizeToFitWidth = true
         
     }
     
-    @IBAction func openURLTapped(_ sender: Any) {
-        guard let url = Foundation.URL(string: "https://shiro-shiro.jp/item/12716.html") else { return }
-        UIApplication.shared.open(url)
-    }
+        @IBAction func openURLTapped(_ sender: Any) {
+            guard let url = Foundation.URL(string: "https://shiro-shiro.jp/item/12716.html") else { return }
+            UIApplication.shared.open(url)
+        }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
